a64f56662954d3848d5625a5eeeb51929eef76bf
yurukatsu <yuruyuru.kaihatsu@gmail.com>
2023-11-03 15:20:16 +0900

    create programs (#3)

---
+++ projects/agent-debating/src/prompts/moderator.py
@@ -0,0 +1,7 @@
+TOPIC_SPECIFIER_PROMPT_TEMPLATE = """{topic}
+
+You are the moderator.
+Please make the topic more specific.
+Please reply with the specified quest in {word_limit} words or less.
+Speak directly to all of the participants: {names}.
+Do not add anything else."""
